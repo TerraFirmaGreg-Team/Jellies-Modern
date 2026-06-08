449248b5f722bb246165500273f46439e7821435
Sakura <sakura.kitsurugi@proton.me>
2026-06-08 14:50:27 +0300

    temp mixin to prevent crash and make gradlew executable on linux

---
+++ src/main/java/team/terrafirmagreg/jellies/mixin/AnyMixin.java
@@ -0,0 +1,9 @@
+package team.terrafirmagreg.jellies.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import team.terrafirmagreg.jellies.Jellies;
+
+@Mixin(value = Jellies.class, remap = false)
+public class AnyMixin {
+}
