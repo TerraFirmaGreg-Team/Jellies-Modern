446086464aca5510d31eb795944aa95e9851410d
Sakura <sakura.kitsurugi@proton.me>
2026-07-09 23:26:14 +0300

    fixed lang again

--- src/main/java/team/terrafirmagreg/jellies/common/data/JelliesItems.java
+++ src/main/java/team/terrafirmagreg/jellies/common/data/JelliesItems.java
@@ -53,29 +53,30 @@ public class JelliesItems {
             .register();
 
     // Basic
-    public static final ItemEntry<ForgeSpawnEggItem> BIOTITE_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.BIOTITE_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> CERTUS_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.CERTUS_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> GLOWBERRY_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.GLOWBERRY_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> HERBAL_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.HERBAL_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> ICE_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.ICE_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> LATEX_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.LATEX_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> LAVA_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.LAVA_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> PENTETIC_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.PENTETIC_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> PHOSPHORUM_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.PHOSPHORUM_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> PLANT_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.PLANT_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> ROCK_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.ROCK_JELLIE, 0x6ce3f5, 0x1ca9eb);
-    public static final ItemEntry<ForgeSpawnEggItem> SPRING_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.SPRING_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> BIOTITE_JELLIE_SPAWN_EGG = registerSpawnEgg("Biotite", JelliesEntities.BIOTITE_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> CERTUS_JELLIE_SPAWN_EGG = registerSpawnEgg("Certus", JelliesEntities.CERTUS_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> GLOWBERRY_JELLIE_SPAWN_EGG = registerSpawnEgg("Glowberry", JelliesEntities.GLOWBERRY_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> HERBAL_JELLIE_SPAWN_EGG = registerSpawnEgg("Herbal", JelliesEntities.HERBAL_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> ICE_JELLIE_SPAWN_EGG = registerSpawnEgg("Ice", JelliesEntities.ICE_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> LATEX_JELLIE_SPAWN_EGG = registerSpawnEgg("Latex", JelliesEntities.LATEX_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> LAVA_JELLIE_SPAWN_EGG = registerSpawnEgg("Lava", JelliesEntities.LAVA_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> PENTETIC_JELLIE_SPAWN_EGG = registerSpawnEgg("Pentetic", JelliesEntities.PENTETIC_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> PHOSPHORUM_JELLIE_SPAWN_EGG = registerSpawnEgg("Phosphorum", JelliesEntities.PHOSPHORUM_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> PLANT_JELLIE_SPAWN_EGG = registerSpawnEgg("Plant", JelliesEntities.PLANT_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> ROCK_JELLIE_SPAWN_EGG = registerSpawnEgg("Rock", JelliesEntities.ROCK_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> SPRING_JELLIE_SPAWN_EGG = registerSpawnEgg("Spring", JelliesEntities.SPRING_JELLIE, 0x6ce3f5, 0x1ca9eb);
 
     // Special
-    public static final ItemEntry<ForgeSpawnEggItem> LILAC_JELLIE_SPAWN_EGG = registerSpawnEgg(JelliesEntities.SPRING_JELLIE, 0x6ce3f5, 0x1ca9eb);
+    public static final ItemEntry<ForgeSpawnEggItem> LILAC_JELLIE_SPAWN_EGG = registerSpawnEgg("Lilac", JelliesEntities.SPRING_JELLIE, 0x6ce3f5, 0x1ca9eb);
 
     // Unique
     // NONE YET
 
-    private static <T extends Mob> ItemEntry<ForgeSpawnEggItem> registerSpawnEgg(EntityEntry<T> entity, int color1, int color2) {
+    private static <T extends Mob> ItemEntry<ForgeSpawnEggItem> registerSpawnEgg(String name, EntityEntry<T> entity, int color1, int color2) {
         return Jellies.REGISTRATE.item("spawn_egg/" + entity.getId().getPath(),
                 (p) -> new ForgeSpawnEggItem(entity, color1, color2, p))
                 .setData(ProviderType.ITEM_MODEL, (ctx, prov) -> prov.withExistingParent(ctx.getName(), ResourceLocation.withDefaultNamespace("item/template_spawn_egg")))
+                .lang(name + " Jellie Spawn Egg")
                 .register();
     }
 }
