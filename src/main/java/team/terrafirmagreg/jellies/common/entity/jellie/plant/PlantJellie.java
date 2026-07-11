e1cb3c67e3214434e511c2cba3afab00976524a7
Sakura <sakura.kitsurugi@proton.me>
2026-07-11 13:55:32 +0300

    py jellie spawn condition, going to delay rock/certus functionality for future.

--- src/main/java/team/terrafirmagreg/jellies/common/entity/jellie/plant/PlantJellie.java
+++ src/main/java/team/terrafirmagreg/jellies/common/entity/jellie/plant/PlantJellie.java
@@ -1,5 +1,7 @@
 package team.terrafirmagreg.jellies.common.entity.jellie.plant;
 
+import net.dries007.tfc.common.blocks.TFCBlocks;
+import net.dries007.tfc.common.blocks.plant.Plant;
 import net.dries007.tfc.common.entities.livestock.TFCAnimal;
 import net.dries007.tfc.common.entities.livestock.TFCAnimalProperties;
 import net.minecraft.core.BlockPos;
@@ -7,13 +9,21 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.ServerLevelAccessor;
+
+import earth.terrarium.adastra.api.planets.Planet;
+import su.terrafirmagreg.core.common.entity.moonrabbit.MoonRabbit;
 
 import team.terrafirmagreg.jellies.Jellies;
 import team.terrafirmagreg.jellies.common.data.JelliesEntities;
@@ -21,6 +31,7 @@ import team.terrafirmagreg.jellies.common.data.JelliesItems;
 import team.terrafirmagreg.jellies.common.data.JelliesTags;
 import team.terrafirmagreg.jellies.common.entity.JellieBase;
 import team.terrafirmagreg.jellies.common.entity.jellie.lava.LavaJellie;
+import team.terrafirmagreg.jellies.common.entity.special.pyritie.PyritieJellie;
 
 public class PlantJellie extends JellieBase {
     public boolean birthLatexJellie = false;
@@ -67,5 +78,25 @@ public class PlantJellie extends JellieBase {
         }
 
         return super.getBreedOffspring(level, other);
+    }
+
+    public InteractionResult mobInteract(Player player, InteractionHand hand) {
+        final ItemStack held = player.getItemInHand(hand);
+
+        if (held.is(TFCBlocks.PLANTS.get(Plant.LILAC).get().asItem()) && this.level().dimension() == Planet.MOON) {
+            boolean hasMoonRabbit = this.level().getEntitiesOfClass(MoonRabbit.class, this.getBoundingBox().inflate(2.0D), rabbit -> rabbit.getMoonVariant().id() == 5).stream().findAny().isPresent();
+
+            if (hasMoonRabbit) {
+                final PyritieJellie pyJellie = this.convertTo(JelliesEntities.PYRITIE_JELLIE.get(), false);
+                if (pyJellie != null && this.level() instanceof ServerLevelAccessor server) {
+                    pyJellie.finalizeSpawn(server, this.level().getCurrentDifficultyAt(blockPosition()), MobSpawnType.CONVERSION, null, null);
+                }
+
+                held.shrink(1);
+                return InteractionResult.SUCCESS;
+            }
+        }
+
+        return super.mobInteract(player, hand);
     }
 }
