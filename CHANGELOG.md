a467fc529c6f22de5e6091e0a4864e956f7e17e3
Sakura <sakura.kitsurugi@proton.me>
2026-08-18 23:30:19 +0300

    bread

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,17 +2,21 @@
 
 ## Unreleased
 
+## [0.1.5] - ???
+### Changes
+- Teto jellie can now eat bread.
+
 ## [0.1.4] - ???
-# Changes
+### Changes
 - Added Teto jellie that was commissioned by Ayzer (Currently not obtainable, but can be spawned in).
 - Biotite jellie now spawns only in completely dark areas.
-# Bugfixes
+### Bugfixes
 - Removed tag-based spawn checking that I forgot to remove.
 
 ## [0.1.3] - 02-08-2026
-## Changes
+### Changes
 - Added a general entity tag to jellies.
-## Bugfixes
+### Bugfixes
 - Lava jellie is now fire immune.
 
 ## [0.1.2] - 31-07-2026
--- src/main/java/team/terrafirmagreg/jellies/common/entity/unique/teto/TetoJellie.java
+++ src/main/java/team/terrafirmagreg/jellies/common/entity/unique/teto/TetoJellie.java
@@ -1,6 +1,9 @@
 package team.terrafirmagreg.jellies.common.entity.unique.teto;
 
 import net.dries007.tfc.common.entities.livestock.TFCAnimal;
+import net.dries007.tfc.common.items.Food;
+import net.dries007.tfc.common.items.TFCItems;
+import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.util.calendar.Calendars;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
@@ -8,6 +11,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 
@@ -70,5 +74,17 @@ public class TetoJellie extends JellieBase {
 
     public ResourceKey<Level> getDimension() {
         return Planet.MOON;
+    }
+
+    public boolean isFood(ItemStack stack) {
+        if (Helpers.isItem(stack, TFCItems.FOOD.get(Food.BARLEY_BREAD).get()) ||
+                Helpers.isItem(stack, TFCItems.FOOD.get(Food.MAIZE_BREAD).get()) ||
+                Helpers.isItem(stack, TFCItems.FOOD.get(Food.OAT_BREAD).get()) ||
+                Helpers.isItem(stack, TFCItems.FOOD.get(Food.RYE_BREAD).get()) ||
+                Helpers.isItem(stack, TFCItems.FOOD.get(Food.RICE_BREAD).get()) ||
+                Helpers.isItem(stack, TFCItems.FOOD.get(Food.WHEAT_BREAD).get()))
+            return true;
+
+        return super.isFood(stack);
     }
 }
